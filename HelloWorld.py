565a79b9e77f46a24df0b46d7560e20b94fb1746
liangyaoxp <liangyaoxp@yeah.net>
2017-06-04 22:43:32 +0800

    modify!

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,14 @@
 import pymysql
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='mysql', charset='UTF8')
-cur = conn.cursor()
-query = "SELECT Host,User FROM user"
-cur.execute(query)
-for i in cur:
-    print(i)
-cur.close()
-conn.close()
+
+def test_mysql():
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='123456', db='mysql', charset='UTF8')
+    cur = conn.cursor()
+    query = "SELECT Host,User FROM user"
+    cur.execute(query)
+    for row in cur:
+        print(row)
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    test_mysql()
